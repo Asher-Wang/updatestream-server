f777c04feeb85e053ad92d94f81238f7aaea0cdc
Asher-Wang <ziheng.wang@hotstar.com>
2021-08-04 16:29:40 +0800

    Asher | Integrate with passport-sdk

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/asher/upstream-server
 
 go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.3 // indirect
+	github.com/hotstar/passport-go v0.0.0-20210803062753-dc8c97eff312 // indirect
+)
--- server.go
+++ server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
-	// "strings"
+
+	passport "github.com/hotstar/passport-go"
 )
 
 var port = 3000
@@ -15,46 +16,33 @@ const (
 	UserTokenAuthenticated = "X-HS-TS-Authenticated"
 )
 
-type Handler struct {
-}
-
-func (h *Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	fmt.Println("Request received, printing all headers....")
-	for name, values := range req.Header {
-		for _, value := range values {
-			fmt.Println(name, value)
-		}
-	}
-	// authenticated := req.Header.Get(strings.ToLower(UserTokenAuthenticated))
-	// if authenticated != "true" {
-	// 	rErr(resp, 403, "unauthenticated")
-	// 	return
-	// }
-	// isTokenRefreshed := req.Header.Get(strings.ToLower(IsUserTokenRefreshed))
-	// if isTokenRefreshed == "true" {
-	// 	refreshedToken := req.Header.Get(strings.ToLower(UserTokenHeader))
-	// 	if refreshedToken == "" {
-	// 		rErr(resp, 403, "Empty passport")
-	// 		return
-	// 	}
-	// 	resp.Header().Add(UserTokenHeader, refreshedToken)
-	// }
-
-	resp.Header().Add("Content-Type", "application/json")
-}
-
 func main() {
 	log.Printf("Auth service (HTTP) running on %d...\n", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), &Handler{})
-	if err != nil {
-		panic(err)
+	r := gin.Default()
+	v1 := r.Group("/v1")
+	v1.Use(passport.PassportMiddleware())
+	{
+		v1.GET("/user", func(c *gin.Context) {
+			user := passport.GetUser(c)
+			userb, err := json.Marshal(user)
+			if err != nil {
+				c.AbortWithError(400, err)
+			}
+			c.JSON(200, gin.H{
+				"user": string(userb),
+			})
+		})
+		v1.GET("/device", func(c *gin.Context) {
+			device := passport.GetDevice(c)
+			deviceb, err := json.Marshal(device)
+			if err != nil {
+				c.AbortWithError(400, err)
+			}
+			c.JSON(200, gin.H{
+				"user": string(deviceb),
+			})
+		})
 	}
-}
-
-func rErr(resp http.ResponseWriter, statusCode int, message string) {
-	resp.WriteHeader(statusCode)
-	resp.Write([]byte(message))
-
-	log.Printf("[Response] %d: %s\n", statusCode, message)
+	r.Run()
 }
