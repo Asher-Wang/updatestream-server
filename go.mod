6bbba2cf23f25cc0915ef7dc91dd6e384919cd23
Zihui Huang <zihui.huang@hotstar.com>
2021-12-22 11:23:35 +0800

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/asher/upstream-server
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.3
-	github.com/hotstar/passport-go v0.0.0-20210820034756-a5412068fcea
+	github.com/gin-gonic/gin v1.7.7
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/hotstar/passport-go v1.0.1
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	passport "github.com/hotstar/passport-go"
+	"github.com/hotstar/passport-go"
 )
 
 var port = 3000
@@ -29,31 +29,18 @@ func main() {
 		})
 	})
 	v1 := r.Group("/v1")
-	v1.Use(passport.PassportMiddleware())
 	{
 		v1.GET("/user", func(c *gin.Context) {
-			user := passport.GetUser(c)
-			c.JSON(200, user)
+			user := passport.GinGetUser(c)
+			c.JSON(200, gin.H{"user": user, "request_headers": c.Request.Header})
 		})
 		v1.GET("/device", func(c *gin.Context) {
-			device := passport.GetDevice(c)
-			c.JSON(200, device)
+			device := passport.GinGetDevice(c)
+			c.JSON(200, gin.H{"device": device, "request_headers": c.Request.Header})
 		})
 		v1.GET("/passport", func(c *gin.Context) {
-			for k, vals := range c.Request.Header {
-				log.Printf("%s", k)
-				for _, v := range vals {
-					log.Printf("\t%s", v)
-				}
-			}
-			passportHeader := c.GetHeader(PassportHeader)
-			pbPassport, err := passport.DecodePassport(passportHeader)
-			if err != nil {
-				c.JSON(400, gin.H{
-					"error": err.Error(),
-				})
-			}
-			c.JSON(200, pbPassport)
+			p := passport.GinGetPassport(c)
+			c.JSON(200, gin.H{"passport": p, "request_headers": c.Request.Header})
 		})
 	}
 	r.Run(":8080")
